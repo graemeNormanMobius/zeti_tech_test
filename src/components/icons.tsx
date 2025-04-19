75e320690cbcd533d53d885684ac51211b9ea08b
graemeNormanMobius <graeme.norman@mobiuscarbon.com>
2025-04-19 21:03:48 +0100

    Update login page || add react-hook-form package

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -100,3 +100,71 @@ export function VerticalDotsIcon({ className }: { className?: string }) {
     </svg>
   );
 }
+
+export function WarningTriangleIcon({ className }: { className?: string }) {
+  return (
+      <svg
+          className={className}
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M1 21h22L12 2zm12-3h-2v-2h2zm0-4h-2v-4h2z"
+              fill="currentColor"
+        />
+    </svg>
+  );
+}
+
+export function VisibilityIcon({ className }: { className?: string }) {
+  return (
+      <svg
+          className={className}
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5m0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3"
+              fill="currentColor"
+        />
+    </svg>
+  );
+}
+
+export function VisibilityOffIcon({ className }: { className?: string }) {
+  return (
+      <svg
+          className={className}
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7M2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2m4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3z"
+              fill="currentColor"
+        />
+    </svg>
+  );
+}
+
+export function CircularSpinningIcon({ className }: { className?: string }) {
+  return (
+      <svg
+          className={className}
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M15.55 5.55 11 1v3.07C7.06 4.56 4 7.92 4 12s3.05 7.44 7 7.93v-2.02c-2.84-.48-5-2.94-5-5.91s2.16-5.43 5-5.91V10zM19.93 11c-.17-1.39-.72-2.73-1.62-3.89l-1.42 1.42c.54.75.88 1.6 1.02 2.47zM13 17.9v2.02c1.39-.17 2.74-.71 3.9-1.61l-1.44-1.44c-.75.54-1.59.89-2.46 1.03m3.89-2.42 1.42 1.41c.9-1.16 1.45-2.5 1.62-3.89h-2.02c-.14.87-.48 1.72-1.02 2.48"
+              fill="currentColor"
+        />
+    </svg>
+  );
+}
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,23 +1,40 @@
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import { Button } from "../components/button.tsx";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { CircularSpinningIcon, VisibilityIcon, VisibilityOffIcon, WarningTriangleIcon } from "../components/icons.tsx";
 
 export function Login() {
     const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        handleLogin(email, password);
+    const [passwordShown, setPasswordShown] = useState<any>({ password: false });
+    const [showLoading, setShowLoading] = useState<boolean>(false);
+    const togglePasswordVisibility = (element: string, visibility: boolean) => {
+        setPasswordShown({ ...passwordShown, [element]: visibility });
     };
 
-    function handleLogin(email: any, password: any) {
+    const preloadedValues = {
+        email: '',
+        password: ''
+    };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<{ email: string; password: string; }>({
+        defaultValues: preloadedValues
+    });
+
+    const onSubmit: SubmitHandler<{ email: string; password: string; }> = data => {
+        setShowLoading(true);
+        // handleLogin(data);
+
         fetch("/api/users")
             .then((response) => response.json())
             .then((users) => {
                 const user = users.find(
-                    (u: any) => u.email === email && u.password === password
+                    (u: any) => u.email === data.email && u.password === data.password
                 );
                 if (user) {
                     const { password, ...userWithoutPassword } = user;
@@ -30,21 +47,115 @@ export function Login() {
                     alert("Invalid credentials");
                 }
             });
-    }
+    };
+
+    // function handleLogin(data: { email: string; password: string }) {
+    //     fetch("/api/users")
+    //         .then((response) => response.json())
+    //         .then((users) => {
+    //             const user = users.find(
+    //                 (u: any) => u.email === data.email && u.password === data.password
+    //             );
+    //             if (user) {
+    //                 const { password, ...userWithoutPassword } = user;
+    //                 Cookies.set('userIdentity', JSON.stringify(userWithoutPassword), { expires: 7 })
+    //
+    //                 if (Cookies.get('userIdentity') !== undefined) {
+    //                     navigate('/');
+    //                 }
+    //             } else {
+    //                 alert("Invalid credentials");
+    //             }
+    //         });
+    // }
 
     return (
-        <div className='mcCard backgroundFadedBoxShadow spacing-md login'>
-            <div className="standardCard">
-                <form onSubmit={handleSubmit}>
-                    <input type="email" width={'400'} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                    <input type="password" width={'400'} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                    <button type="submit">Login</button>
-                </form>
+        <div className="gridContainer">
+            <div className='mcCard backgroundFadedBoxShadow spacing-md login'>
+                <div className="standardCard">
+
+                    <form className="signInForm customForm" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group">
+                            <label className="form-label bodyXSmallEmp" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                className="form-control bodySmall"
+                                id="email"
+                                {...register('email', {
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/,
+                                        message: 'Please enter a valid email address'
+                                    }
+                                })}
+                            />
+                            <ErrorMessage
+                                errors={errors}
+                                name="email"
+                                render={({ message }) => (
+                                    <div className="bodyXSmall formErrorContainer">
+                                        <WarningTriangleIcon className="formErrorIcon" />
+                                        <span>{message}</span>
+                                    </div>
+                                )}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label className="form-label bodyXSmallEmp" htmlFor="password">
+                                Password
+                            </label>
+                            <span className="inputToggle"
+                                  onClick={() => togglePasswordVisibility('password', !passwordShown.password)}>
+                              {passwordShown.password ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                            </span>
+                            <input
+                                type={passwordShown.password ? 'text' : 'password'}
+                                className="form-control visibilityToggle bodySmall"
+                                id="password"
+                                {...register('password', { required: 'This is a required field' })}
+                            />
+                            <ErrorMessage
+                                errors={errors}
+                                name="password"
+                                render={({ message }) => (
+                                    <div className="bodyXSmall formErrorContainer">
+                                        <WarningTriangleIcon className="formErrorIcon" /> {message}
+                                    </div>
+                                )}
+                            />
+                        </div>
+
+                        <div className="modalFooter modal-buttonGroup">
+                            {!showLoading && (
+                                <Button
+                                    hasIcon="noIcon"
+                                    size="md"
+                                    state="default"
+                                    type="accent"
+                                    value="Sign in "
+                                    isSubmit={true}
+                                />
+                            )}
+
+                            {showLoading && (
+                                <Button
+                                    hasIcon="leading"
+                                    size="lg"
+                                    state="disabled"
+                                    type="accent"
+                                    value={
+                                        <span className="buttonAfter">
+                                            <CircularSpinningIcon className="loginLoader" />
+                                        </span>
+                                    }
+                                    isSubmit={false}
+                                />
+                            )}
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
-
-    // return (
-    //     <div className="h3">Login page</div>
-    // )
 }
